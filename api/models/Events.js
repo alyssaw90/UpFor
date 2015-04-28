1309a51e13d4245c7ebfbf33851d978ff0413aa2
Alyssa <alyssaw90@gmail.com>
2015-04-27 21:51:31 -0700

    event model attributes added

--- api/models/Events.js
+++ api/models/Events.js
@@ -9,6 +9,52 @@ module.exports = {
 
   attributes: {
 
+  	eventName: {
+  		type: 'string'
+  	},
+  	venueName: {
+  		type: 'string'
+  	},
+  	photo: {
+  		type: 'string'
+  	},
+  	price: {
+  		type: 'string'
+  	},
+  	address: {
+  		type: 'string'
+  	},
+  	eventDescription: {
+  		type: 'string'
+  	},
+  	venueDescription: {
+  		type: 'string'
+  	},
+  	ticketLink: {
+  		type: 'string'
+  	},
+  	startTime: {
+  		type: 'string'
+  	},
+  	endTime: {
+  		type: 'string'
+  	},
+  	startDate: {
+  		type: 'string'
+  	},
+  	endDate: {
+  		type: 'string'
+  	},
+  	interest: {
+  		type: 'string'
+  	},
+  	neighborhood: {
+  		type: 'string'
+  	},
+  	city: {
+  		type: 'string'
+  	}
+
   }
 };
 

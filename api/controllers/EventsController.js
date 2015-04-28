4f51e62ff2751b8de6cf103ca8064fa723884cc0
Alyssa <alyssaw90@gmail.com>
2015-04-27 22:07:12 -0700

    event controller create function added

--- api/controllers/EventsController.js
+++ api/controllers/EventsController.js
@@ -7,7 +7,29 @@
 
 module.exports = {
 
-	
+	create: function(req, res){
+		var eventData = {
+			eventName: req.body.eventName,
+			venueName: req.body.venueName,
+			photo: req.body.photo,
+			address: req.body.address,
+			eventDescription: req.body.eventDescription,
+			venueDescription: req.body.venueDescription,
+			ticket: req.body.ticket,
+			startTime: req.body.startTime,
+			endTime: req.body.endTime,
+			startDate: req.body.startDate,
+			endDate: req.body.endDate,
+			interest: req.body.interest,
+			neighborhood: req.body.neighborhood,
+			city: req.body.city
+		}
+
+		Events.create(eventData).exec(function(err, event){
+			if(err) res.send(400, err);
+			res.send(event)
+		});
+	};	
 	
 };
 
